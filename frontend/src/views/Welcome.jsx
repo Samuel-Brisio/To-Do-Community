ff7ed9108a6cf95420d7750a42abe9e5aa4409f8
VKenji-pa <84997899+VKenji-pa@users.noreply.github.com>
2023-05-05 04:17:51 -0300

    Update no Welcome.jsx

--- frontend/src/views/Welcome.jsx
+++ frontend/src/views/Welcome.jsx
@@ -9,27 +9,47 @@ function Welcome() {
             <form className="card shadow-lg flex rounded w-1/2">
                 <Tabs defaultActiveKey="Login" variant="pills" fill>
                     <Tab eventKey="Login" title="Login" className="p-5">
-                        
-                            <h1 className="text-3xl text-center">
-                                Login VIPList
-                            </h1>
-                            <div className="text-justify px-5">
-                                {/* Nome */}
-                                <label className="py-2" htmlFor="e-mail">
-                                    E-mail:
-                                </label>
-                                <input className="form-control" type="text" name="name" placeholder="Digite seu nome"/>
-                                <label className="py-2" htmlFor="name">
-                                    Senha:
-                                </label>
-                                <input className="form-control" type="password" name="senha" placeholder="Digite sua senha"/>
-                                <div className="pt-3 text-center">
-                                    <button className="btn customButton">Entrar</button>
-                                </div>
+                        <h1 className="text-3xl text-center">
+                            Login VIPList
+                        </h1>
+                        <div className="text-justify px-5">
+                            {/* Nome */}
+                            <label className="py-2" htmlFor="e-mail">
+                                E-mail:
+                            </label>
+                            <input className="form-control" type="text" name="name" placeholder="Digite seu e-mail"/>
+                            <label className="py-2" htmlFor="name">
+                                Senha:
+                            </label>
+                            <input className="form-control" type="password" name="senha" placeholder="Digite sua senha"/>
+                            <div className="pt-3 text-center">
+                                <button className="btn customButton">Entrar</button>
                             </div>
-                        
+                        </div>
                     </Tab>
-                    <Tab eventKey="Cadastro" title="Cadastro"></Tab>
+                    <Tab eventKey="Cadastro" title="Cadastro" className="p-5">
+                        <h1 className="text-3xl text-center">
+                            Cadastre-se no VIPList
+                        </h1>
+                        <div className="text-justify px-5">
+                            {/* Nome */}
+                            <label className="py-2" htmlFor="nome">
+                                Nome:
+                            </label>
+                            <input className="form-control" type="text" name="name" placeholder="Digite seu Nome"/>
+                            <label className="py-2" htmlFor="email">
+                                E-mail:
+                            </label>
+                            <input className="form-control" type="text" name="email" placeholder="Digite seu e-mail"/>
+                            <label className="py-2" htmlFor="name">
+                                Senha:
+                            </label>
+                            <input className="form-control" type="password" name="senha" placeholder="Digite sua senha"/>
+                            <div className="pt-3 text-center">
+                                <button className="btn customButton">Cadastrar</button>
+                            </div>
+                        </div>
+                    </Tab>
                 </Tabs>
                 </form>
             </div>    
