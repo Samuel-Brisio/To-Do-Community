9e113ea9fea8199f874cac4a4a40661042a8f7f0
Samuel-Brisio <samuelbrisio@outlook.com>
2023-05-07 18:33:42 -0300

    Cors fixed

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express, { Express, Request, Response } from "express";
-import * as cors from 'cors';
+import cors from "cors";
 import { useRoutes } from './routes';
 import { listRoutes } from './routes';
 import { itemRoutes } from './routes';
@@ -11,7 +10,7 @@ import bodyParser from 'body-parser';
 const port = 8080;
 
 const app: Express = express();
-const cors = require('cors');
+app.use(cors)
 
 app.use(cors({
   origin: 'http://localhost:3000',
