51d4425a0f7c966bc81fc534df589bf7083c57c9
deborahsag <85948728+deborahsag@users.noreply.github.com>
2023-05-08 11:29:16 -0300

    tentativa de fazer o submit de nova lista.

--- frontend/src/components/AddListForm.jsx
+++ frontend/src/components/AddListForm.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import api from "../Services/api.js";
 
 const AddList = (props) => {
+    const [listName, setListName] = useState("");
+
+    function handleListNameChange(event){
+        setListName(event.target.value)
+    }
+    function handleSubmit(event){
+        console.log('Handle Submit OK!');
+        
+        api.get("/users", {listName})
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log("erro: ", err);
+            })
+    }
+
     return (
         <>
             <div
@@ -22,8 +41,12 @@ const AddList = (props) => {
                         placeholder={props.placeholder}
                         aria-label="name"
                         aria-describedby="basic-addon2"
+                        type="text"
+                        name="listName"
+                        value={listName}
+                        onChange={handleListNameChange}
                     />
-                    <Button variant="dark" id="button-addon2">
+                    <Button variant="dark" id="button-addon2" onClick={handleSubmit}>
                         +
                     </Button>
                 </InputGroup>
